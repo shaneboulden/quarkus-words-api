38e2c01fd9edf96f85a304d99ee34384dd2c6267
shaneboulden <shane.boulden@gmail.com>
2019-06-11 04:36:55 +1000

    use a cache on a timer

---
+++ src/main/java/org/rock/rest/CachedResponse.java
@@ -0,0 +1,54 @@
+package org.rock.rest;
+
+import java.util.concurrent.atomic.AtomicReference;
+import javax.enterprise.context.ApplicationScoped;
+import io.quarkus.scheduler.Scheduled;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+@ApplicationScoped
+public class CachedResponse {
+
+  private AtomicReference<StringBuffer> cache_data  = new AtomicReference<StringBuffer>();
+
+  public CachedResponse() {
+    StringBuffer bfr = new StringBuffer("");
+    cache_data.set(bfr);
+  }
+
+  public StringBuffer get() {
+    return cache_data.get();
+  }
+
+  @Scheduled(every="10s")
+  public void get_data() {
+
+      try {
+            URL url = new URL("http://grafzahl/data");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setConnectTimeout(10000);
+            con.setReadTimeout(10000);
+
+            BufferedReader in = new BufferedReader(
+            new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+              content.append(inputLine);
+            }
+            in.close();
+            cache_data.set(content);
+      }
+      catch(Exception ex) {
+          System.out.println(ex);
+      }
+   }
+}
--- src/main/java/org/rock/rest/DataResource.java
+++ src/main/java/org/rock/rest/DataResource.java
@@ -5,6 +5,7 @@ import java.util.LinkedHashMap;
 import java.util.Set;
 import java.util.Random;
 
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -20,19 +21,15 @@ import com.google.gson.JsonArray;
 
 import com.google.gson.stream.JsonReader;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 import java.util.Arrays;
 
 @Path("/api/v1/words")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class DataResource {
+
+    @Inject
+    CachedResponse response;
 
     public DataResource() {
 
@@ -45,23 +42,9 @@ public class DataResource {
         // make a request to the API
         // TODO change this to an environment variable
         try {
-            URL url = new URL("http://grafzahl:8080/data");
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            con.setRequestProperty("Content-Type", "application/json");
-            con.setConnectTimeout(10000);
-            con.setReadTimeout(10000);
-
-            BufferedReader in = new BufferedReader(
-            new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuffer content = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-              content.append(inputLine);
-            }
-            in.close();
 
             // now let's parse the JSON to data points
+            StringBuffer content = response.get();
             JsonObject jsonObject = new JsonParser().parse(content.toString()).getAsJsonObject();
             JsonElement categories = jsonObject.get("categories");
             JsonElement words = jsonObject.get("data").getAsJsonArray().get(0);
